3e6a13e06440bed39e62408ff4b11124d2642f44
Reinaldo2619 <161092816+Reinaldo2619@users.noreply.github.com>
2024-05-08 19:02:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@mainc/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "banco.h"
+
+int main() {
+    Cliente clientes[MAX_CLIENTES];
+    int total_clientes = carregarClientes(clientes);
+
+    int opcao;
+    do {
+        printf("\nMenu de Opcoes:\n");
+        printf("1. Novo cliente\n");
+        printf("2. Apagar cliente\n");
+        printf("3. Listar clientes\n");
+        // Adicione aqui as outras opções do menu...
+        printf("0. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                novoCliente(clientes, &total_clientes);
+                break;
+            case 2:
+                apagarCliente(clientes, &total_clientes);
+                break;
+            case 3:
+                listarClientes(clientes, total_clientes);
+                break;
+            // Implemente aqui os casos das outras opções do menu...
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+        }
+    } while (opcao != 0);
+
+    return 0;
+}
